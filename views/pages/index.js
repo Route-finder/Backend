37a2bf9d98b932294477ad073544dbb33b9f4740
Isaac-list <listis01@luther.edu>
2022-03-15 09:59:20 -0500

    I just want this to work

--- views/pages/index.js
+++
@@ -1,7 +0,0 @@
-let nameInput = document.getElementById("username");
-
-function getName() {
-    console.log("getName Running")
-    let cookie = 'name=' + nameInput.value + '; SameSite=None; Secure';
-    document.cookie = cookie;
-}
