a3cf3f774c23721d15ea609a849c6da5ff510e9b
Isaac-list <listis01@luther.edu>
2022-04-01 19:58:40 -0500

    test expressjs routes, basic API e.p.

--- test/test.js
+++ test/test.js
@@ -3,12 +3,51 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("Unit testing the Backend API", function () {
+  it("Basic API Route Returns JSON", function (done) {
+    request(app)
+      .get('/api')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+
   it("Returns the Booklist as JSON", function (done) {
     request(app)
       // include query parameter in request URL
       .get('/api/books?name=Isaac')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+/*
+  it("Search route returns JSON", function (done) {
+    this.timeout(3500);
+    request(app)
+      .post('/api/search')
+      .send({isbn: '9780380807345', name: 'Isaac'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+  */
+});
+
+describe("Unit testing the Express.js Routes", function () {
+  it("Homepage Returns a successful response", function (done) {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+
+  it("Search Page Returns a successful response", function (done) {
+    request(app)
+      .get('/add')
+      .expect(200, done);
+  });
+
+  it("Route Page Returns a successful response", function (done) {
+    request(app)
+      .get('/route')
       .expect(200, done);
   });
 });
