1fef18f694d0f3450fa7c37f38e3a472a4e7899b
Isaac-list <listis01@luther.edu>
2022-04-01 19:32:46 -0500

    implement the first test (booklist)

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
 const assert = require("assert");
 const request = require("supertest");
-// This pulls the whole server, but we really just want the "app" component
-// Consider how to separate out the "app" from the rest of the server
 const app = require("../app");
 
-// TODO: Learn how to test Express.js routes
-describe("Unit testing the books API route", function () {
-  it("Should return a 200 success value", function () {
-    return 0;
+describe("Unit testing the Backend API", function () {
+  it("Returns the Booklist as JSON", function (done) {
+    request(app)
+      // include query parameter in request URL
+      .get('/api/books?name=Isaac')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
   });
 });
